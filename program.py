62dfd788c1ccfeaf17d749718fd07b07fa12339a
salomon <salomon.olivera@hotmail.com>
2018-09-03 21:09:36 -0500

    Calculator

---
+++ program.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  3 20:52:18 2018
+
+@author: Salomón Olivera Abud
+"""
+
+# =============================================================================
+# import fileinput
+# 
+# lines = []
+# for line in fileinput.input():
+#     lines.append(line)
+#     
+# suma = lines[0] + lines[1]
+# =============================================================================
+
+print('Hello World')
