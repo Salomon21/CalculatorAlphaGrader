aee8eb3264afe021b5c1af819949d174a4136c13
salomon <salomon.olivera@hotmail.com>
2018-09-03 21:11:32 -0500

    run script

--- program.py
+++ program.py
@@ -5,14 +5,13 @@ Created on Mon Sep  3 20:52:18 2018
 @author: Salomón Olivera Abud
 """
 
-# =============================================================================
-# import fileinput
-# 
-# lines = []
-# for line in fileinput.input():
-#     lines.append(line)
-#     
-# suma = lines[0] + lines[1]
-# =============================================================================
+import fileinput
 
-print('Hello World')
+lines = []
+for line in fileinput.input():
+    lines.append(line)
+    
+suma = lines[0] + lines[1]
+
+print(suma)
+    
